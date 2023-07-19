b3976b255bd53f686807a95814edfc0d32de2be3
nburwick <121465238+nburwick@users.noreply.github.com>
2023-07-19 10:31:00 -0700

    Commit to Main

--- SongHTML/app.py
+++ SongHTML/app.py
@@ -3,10 +3,7 @@ from flask import request
 from flask_cors import CORS
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-<<<<<<< HEAD
 from spotipy.cache_handler import MemoryCacheHandler
-=======
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import pandas as pd
@@ -22,10 +19,7 @@ sp_uri = sp_uri
 os.environ['SPOTIPY_CLIENT_ID'] = cid
 os.environ['SPOTIPY_CLIENT_SECRET'] = secret
 os.environ['SPOTIPY_REDIRECT_URI'] = sp_uri
-<<<<<<< HEAD
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret, cache_handler=MemoryCacheHandler())
-=======
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
 
 # Create mongo
 mdb_uri = mdb_uri
@@ -34,7 +28,6 @@ server = ServerApi('1')
 mongo = MongoClient(mdb_uri, server_api=server)
 database = mongo['targ_data']
 targ_songs = database['song_data']
-<<<<<<< HEAD
 
 
 #Define DataBase Search
@@ -42,15 +35,6 @@ def in_mongo(track_name, artists):
     try:    
         result = list(targ_songs.find({'track_name' : track_name, 'artists' : artists}))
         if len(result) > 0:
-=======
-songs = list(targ_songs.find({}))
-
-#Define DataBase Search
-def in_mongo(track_id):
-    try:    
-        result = list(targ_songs.find({'track_id' : track_id}))
-        if len(result) != 0 or pd.isnull(result) == False:
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
             return True
         else:
             return False
@@ -73,15 +57,9 @@ def home():
 #this is loaded when user enter song name, etc
 @app.route('/submit', methods=['POST'])
 def submit():
-<<<<<<< HEAD
     songs = list(targ_songs.find({}))
     #Initiate Spotify Client
     spotify = spotipy.Spotify(auth_manager=client_credentials_manager)
-=======
-    #Initiate Spotify Client
-    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-    spotify = spotipy.Spotify(auth_manager=client_credentials_manager, retries=1, requests_timeout=3)
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
 
     # Capture user input
     songName = request.form.get('songName')
@@ -97,7 +75,6 @@ def submit():
     explicit = result['tracks']['items'][0]['explicit']
     pop = result['tracks']['items'][0]['popularity']
     name = result['tracks']['items'][0]['name']
-<<<<<<< HEAD
     artists = []
     track_arts = result['tracks']['items'][0]['artists']
     for art in track_arts:
@@ -107,29 +84,16 @@ def submit():
     
     # Test MongoDB for DataPoint
     if in_mongo(name, artists) == False:
-=======
-    track_art = result['tracks']['items'][0]['artists'][0]['name']
-    url = result['tracks']['items'][0]['external_urls']['spotify']
-    
-    # Test MongoDB for DataPoint
-    if in_mongo(songID) == False:
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
         # in_mongo returned False Add Song to MongoDB with Attributes
         traits_dictionary = spotify.audio_features(songID)[0]
         traits_dictionary['popularity'] = pop
         traits_dictionary['explicit'] = explicit
         traits_dictionary['track_name'] = name
         traits_dictionary['track_url'] = url
-<<<<<<< HEAD
         traits_dictionary['artists'] = artists
         traits_dictionary['track_id'] = songID
         traits_dictionary['preview_url'] = preview
         keep_traits = ['track_id', 'track_name', 'artists', 'track_url', 'preview_url',
-=======
-        traits_dictionary['artists'] = track_art
-        traits_dictionary['track_id'] = songID
-        keep_traits = ['track_id', 'artists', 'track_name',
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
                        'popularity', 'duration_ms', 'explicit', 'danceability',
                        'energy', 'key', 'loudness', 'mode','speechiness',
                        'acousticness','instrumentalness','liveness','valence',
@@ -139,19 +103,11 @@ def submit():
         delete_list = set(traits_dictionary.keys()) - set(keep_traits)
         for trait in delete_list:
             del traits_dictionary[trait]
-<<<<<<< HEAD
         
         # Push Song into MongoDB to "Smarten" Model    
         targ_songs.insert_one(traits_dictionary)
         print("NEW TRACK ADDED")
         print(traits_dictionary)
-=======
-            
-        # Push Song into MongoDB to "Smarten" Model    
-        targ_songs.insert_one(traits_dictionary)
-        print("NEW TRACK ADDED")
-        global songs 
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
         songs = list(targ_songs.find({}))
 
     else:
@@ -162,7 +118,6 @@ def submit():
     # vectorize values for Machine Learning 
     for trait in traits_dictionary.keys():
         traits_dictionary[trait] = [traits_dictionary[trait]]
-<<<<<<< HEAD
         
     searched = pd.DataFrame(traits_dictionary)
     
@@ -178,18 +133,11 @@ def submit():
     
     searched = searched[['track_name', 'artists', 'preview']].to_html(classes='mystyle', header='true',index=False, justify='left', escape=False)
     search="<h1>SongVue Search</h1>" + searched
-=======
-
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
     # Now, pass songID, Song DB, Song Attributes, and number of recommendations to ML Model Function   
     result = songRecommender(songID, songs, traits_dictionary, recSongs, explicit_filter)
     
     # Return Recommended Songs as Pandas DataFrame
-<<<<<<< HEAD
     return render_template('songs.html', search=[search],tables=[result.to_html(classes='mystyle',header='true', 
-=======
-    return render_template('songs.html', tables=[result.to_html(classes='mystyle',header='true', 
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
                                                                 index=True, justify='left', escape=False)])
     
 # Run Application
--- SongHTML/songRecommender.py
+++ SongHTML/songRecommender.py
@@ -25,10 +25,6 @@ def songRecommender(track_id, mongoDB, traits_dictionary, num_recs, explicit):
     # Convert DataBased Songs into a DataFrame
     original_df = pd.json_normalize(mongoDB)
     original_df = original_df[original_df['track_id'] != track_id].reset_index(drop=True)
-<<<<<<< HEAD
-=======
-    print(f"{len(mongoDB) - original_df.shape[0]} tracks removed")
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
     
     # Filter Explicit?
     if explicit == "0":
@@ -36,24 +32,17 @@ def songRecommender(track_id, mongoDB, traits_dictionary, num_recs, explicit):
       original_df = original_df[~original_df['explicit']].reset_index(drop=True)
 
     # Extract Song Qualities
-<<<<<<< HEAD
     song_q = original_df[['danceability', 'energy', 'key',
                           'loudness', 'mode', 'speechiness',
                           'acousticness','instrumentalness',
                           'liveness', 'valence', 'tempo',
                           'time_signature']].dropna().reset_index(drop=True)
-=======
-    song_q = original_df.drop(columns=['track_id', 'artists', 'track_name', 'duration_ms', 'track_url', 'popularity', 'explicit','_id'], errors='ignore').dropna().reset_index(drop=True)
-    
-    # print(song_q.info())
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
 
     # Establish KNN Model
     knn = NearestNeighbors(n_neighbors=int(num_recs))
     knn.fit(song_q)
 
     # Convert Searched Song Traits
-<<<<<<< HEAD
     search = pd.DataFrame(traits_dictionary)
     search = search[['danceability', 'energy', 'key',
                      'loudness', 'mode', 'speechiness',
@@ -82,24 +71,3 @@ def songRecommender(track_id, mongoDB, traits_dictionary, num_recs, explicit):
     
     print(result_df[['track_name', 'artists']])
     return result_df[['Track Name', 'Artists', 'Preview']]
-=======
-    test = pd.DataFrame(traits_dictionary)
-    test = test.drop(columns=['track_id', 'artists', 'track_name', 'duration_ms', 'track_url', 'popularity', 'explicit', '_id'], errors='ignore')
-    
-    # Find Similar Songs from KNN Model
-    result = knn.kneighbors(test)[1].tolist()[0]
-    
-    # Filter DataBase Songs by Result Indices
-    result_df = original_df.iloc[result]
-    
-    # Create Clickable Links & Clean Artists
-    for index, track in result_df.iterrows():
-      link = track['track_url']
-      name = track['track_name']
-      result_df.loc[index,'track_name'] = f'<a href={link}>{name}</a>'
-      arts = track['artists'].strip("][").split(", ")
-      result_df.loc[index, 'artists'] = ", ".join(arts).replace("'","")
-    
-    result_df = result_df[['track_name', 'artists']].reset_index(drop=True)
-    return result_df
->>>>>>> e3dce1ed9c6d71ad67025bd28c561c3dfadafaed
