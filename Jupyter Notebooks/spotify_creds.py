3b531bb4466fd383231ccc823cc30a68a06e31a9
nburwick <121465238+nburwick@users.noreply.github.com>
2024-07-24 13:12:56 -0700

    changing database cluster

--- Jupyter Notebooks/spotify_creds.py
+++ Jupyter Notebooks/spotify_creds.py
@@ -1,4 +1,4 @@
 cid ="e5ee662214304d6fb8f821a2d7f4787e"
 secret = "aa3cdc18f3034454a56b77f785c360ce"
 sp_uri = "http://localhost:8080"
-mdb_uri = 'mongodb+srv://nburwick:Swim_Fast01@cluster0.nvujnf4.mongodb.net/?retryWrites=true&w=majority'
+mdb_uri = 'mongodb+srv://admin:Y1GCuTuF9HltvXrJ@wedding-app.f9oj5ol.mongodb.net/'
